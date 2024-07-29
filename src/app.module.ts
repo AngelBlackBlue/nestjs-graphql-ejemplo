20a370a9557667c0ffa7f537f5837d0671e29843
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2024-07-29 13:13:32 -0300

    type orm y mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
+import { DatabaseModule } from './database/database.module';
+import { GraphqlModule } from './graphql/graphql.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-    PostsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    GraphqlModule,
+    DatabaseModule,
+    PostsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
